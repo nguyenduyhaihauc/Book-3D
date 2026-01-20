86a00ae37e0c9a1d9e6124dba8e2c3fe91d12bf3
nguyenduyhaihauc <nguyenduyhaihauc@gmail.com>
2026-01-20 17:12:44 +0700

    xóa bóng bìa sách

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -98,12 +98,9 @@ pages.forEach((page) => {
 
 // Load và hiển thị ảnh trong sách
 const Page = ({ number, front, back, page, opened, bookClosed, ...props }) => {
-  const [picture, picture2, pictureRoughness] = useTexture([
+  const [picture, picture2] = useTexture([
     `/textures/${front}.jpg`,
     `/textures/${back}.jpg`,
-    ...(number === 0 || number === pages.length - 1
-      ? [`/textures/book-cover-roughness.jpg`]
-      : []),
   ]);
   picture.colorSpace = picture2.colorSpace = SRGBColorSpace;
   const group = useRef();
@@ -134,26 +131,14 @@ const Page = ({ number, front, back, page, opened, bookClosed, ...props }) => {
       new MeshStandardMaterial({
         color: whiteColor,
         map: picture,
-        ...(number === 0
-          ? {
-              roughnessMap: pictureRoughness,
-            }
-          : {
-              roughness: 0.1,
-            }),
+        roughness: 0.1,
         emissive: emissiveColor,
         emissiveIntensity: 0,
       }),
       new MeshStandardMaterial({
         color: whiteColor,
         map: picture2,
-        ...(number === pages.length - 1
-          ? {
-              roughnessMap: pictureRoughness,
-            }
-          : {
-              roughness: 0.1,
-            }),
+        roughness: 0.1,
         emissive: emissiveColor,
         emissiveIntensity: 0,
       }),
